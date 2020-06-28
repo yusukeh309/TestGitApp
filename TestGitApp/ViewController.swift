e5940e62755b1b5b3a1549dbb42854884ee95ee6
yusukeh309 <elmo.15vc@gmail.com>
2020-06-28 19:01:36 +0900

    TestGitApp用のRepository作成

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by Uske on 2020/06/28.
+//  Copyright © 2020 Uske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("TestGitApp")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
